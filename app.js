845ecea22c57956b7ae24f53e4a0434452c6b774
groundmobil <152173861+groundmobil@users.noreply.github.com>
2024-02-09 11:38:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,11 +6,9 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const allowedOrigins = [
-    'http://localhost:3000', 
-    'https://www.diggajmotors.com/',
-];
-
+const corsOptions = {
+    origin: '*',
+};
 const corsOptions = {
     origin: function (origin, callback) {
         if (!origin || allowedOrigins.includes(origin)) {
